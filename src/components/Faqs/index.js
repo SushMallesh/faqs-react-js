9009c44dd82d549241650cb02a6a53fb6b2c165c
Sushma <113226796+SushMallesh@users.noreply.github.com>
2022-11-15 00:27:42 +0530

    Update index.js

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -3,18 +3,26 @@ import FaqItem from '../FaqItem'
 import './index.css'
 
 class Faqs extends Component {
-  state = {isShowAnswer: false, activeId: 0}
+  state = {activeId: []}
 
   onShowAnswer = id => {
-    this.setState(prevState => ({
-      isShowAnswer: !prevState.isShowAnswer,
-      activeId: id,
-    }))
+    this.setState(prevState => {
+      const {activeId} = prevState
+      if (activeId.includes(id)) {
+        return {
+          activeId: activeId.filter(eachId => eachId !== id),
+        }
+      }
+      return {
+        activeId: [...activeId, id],
+      }
+    })
   }
 
   render() {
-    const {isShowAnswer, activeId} = this.state
     const {faqsList} = this.props
+    const {activeId} = this.state
+
     return (
       <div className="app-container">
         <div className="faqs-container">
@@ -25,8 +33,7 @@ class Faqs extends Component {
                 eachFaq={eachFaq}
                 key={eachFaq.id}
                 onShowAnswer={this.onShowAnswer}
-                isAnswerShown={isShowAnswer}
-                isActive={activeId === eachFaq.id}
+                isActive={activeId.includes(eachFaq.id)}
               />
             ))}
           </ul>
